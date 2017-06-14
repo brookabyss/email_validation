0cd73d875641005fc671d7080ce693efa1c5f794
Brook Kebede <brookabyss@gmail.com>
2017-06-13 17:58:31 -0700

    Able to show emails on the show page, able to query back and forth with the database

--- server.py
+++ server.py
@@ -20,11 +20,17 @@ def create():
     if len(email_query) >1 :
         flash('Email is not valid','red')
     else:
-        flash('The email address you entered {} is valid. Thank you!'.format(current_email), 'green')
+        flash('The email address you entered "{}" is valid. Thank you!'.format(current_email) , 'green')
         query="INSERT INTO emails (email,created_at,updated_at) VALUES (:email,now(),now())"
         data={
             "email": current_email
         }
         mysql.query_db(query,data)
+        return redirect('/success')
     return redirect('/')
+@app.route('/success')
+def show():
+    emails=mysql.query_db("SELECT email, DATE_FORMAT(created_at,'%m %d %Y %h:%i %p') AS created_at FROM emails")
+    return render_template('show.html',emails=emails)
+
 app.run(debug=True)
