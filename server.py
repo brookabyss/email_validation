5620dc71d8d03e9381236db3314a60569baf23a6
Brook Kebede <brookabyss@gmail.com>
2017-06-13 17:07:45 -0700

    Able to connect Database and insert data. Logic to check if email exists, display on index remaining

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, render_template, flash,request
+from mysqlconnection import MySQLConnector
+app=Flask(__name__)
+mysql=MySQLConnector(app,'friends')
+app.secret_key="123"
+@app.route('/')
+def index():
+    emails=mysql.query_db("SELECT * FROM emails")
+    print emails
+    return render_template('index.html')
+@app.route('/create', methods=['POST'])
+def create():
+    print request.form['email']
+    current_email=request.form['email']
+    query="SELECT e.email FROM emails e Where email=:email"
+    data={
+        "email": current_email
+    }
+    email_query=mysql.query_db(query,data)
+    if len(email_query) >1 :
+        flash('Email is not valid','red')
+    else:
+        flash('The email address you entered {} is valid. Thank you!'.format(current_email), 'green')
+        query="INSERT INTO emails (email,created_at,updated_at) VALUES (:email,now(),now())"
+        data={
+            "email": current_email
+        }
+        mysql.query_db(query,data)
+    return redirect('/')
+app.run(debug=True)
